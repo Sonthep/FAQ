a6a4c13aee8d6a14bdfd3d6f252e78d66d650a44
thepza <gan14tap@gmail.com>
2022-08-28 02:33:42 +0700

    add vite and tailwind

---
+++ vite/tailwind.config.cjs
@@ -0,0 +1,29 @@
+
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'dark-blue': 'hsl(238, 29%, 16%)',
+      'soft-red': 'hsl(14, 88%, 65%)',
+      'soft-violet': 'hsl(273, 75%, 66%)',
+      'soft-blue': 'hsl(240, 73%, 65%)',
+      'grayish-blue': 'hsl(237, 12%, 33%)',
+      'dark-grayish-blue': 'hsl(240, 6%, 50%)',
+      'light-grayish': 'hsl(240, 5%, 91%)',
+      'light-grayish': 'hsl(240, 5%, 91%)',
+      'white1': 'rgb(255 255 255)',
+
+
+    },
+    extend: {},
+    fontFamily: {
+    }
+  },
+  plugins: [],
+}
